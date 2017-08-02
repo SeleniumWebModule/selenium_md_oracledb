be4bceed2cf8215227981d6fec4859179d894df2
Eduardo Dicarte <eduardo@rjconsultores.com.br>
2017-08-02 04:44:10 -0300

    Configuração da rede (TNS) do oracle via template

--- manifests/oracle/configure/network.pp
+++ manifests/oracle/configure/network.pp
@@ -1,18 +1,22 @@
 class selenium_md_oracledb::oracle::configure::network {
-	$path_respfile = "/opt/oradb/database/response/netca.rsp"
+  require selenium_md_oracledb::oracle::install::doinstall
+
+  $path_rsp = "/opt/oradb/database/response"
+	$path_netca = "${path_rsp}/netca.rsp"
+  $oracleHome = "/opt/oradb/u01/app/oracle/product/11.2.0/dbhome_1"
 	
 	file {'netca.rsp':
     	ensure  => file,
-    	path    => '/opt/oradb/database/response/netca.rsp',
+    	path    => $path_netca,
     	content => template("selenium_md_oracledb/netca.rsp.erb"),
-    	ignore => 'ls /opt/oradb/u01/'
-  	} ->
+    	ignore => "ls ${oracleHome}"
+  } ->
 
-	exec { 'configure_oracle_network':
-  		command      => "sh netca /silent /responseFile ${path_respfile}",
-  		path         => '/usr/bin',
-  		cwd          => '/opt/oradb/u01/app/oracle/product/11.2.0/dbhome_1/bin/',
-  		user         => 'oracle',
-  		unless       => 'ls /opt/oradb/u01/'
-  	}
+  exec { 'configure_oracle_network':
+      command      => "sh netca /silent /responseFile ${path_netca}",
+      path         => '/usr/bin',
+      cwd          => "${oracleHome}/bin/",
+      user         => 'oracle',
+      timeout      =>  0
+  }
 }
