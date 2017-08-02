708509a702d71bb576d9a3d80060905ad806c632
Eduardo Dicarte <eduardo@rjconsultores.com.br>
2017-08-02 04:45:48 -0300

    Separação de responsabilidades e inclusão de parametro para que a instalação não fosse executada em background, necessário para o controle do puppet/ docker

--- manifests/oracle/install/doinstall.pp
+++ manifests/oracle/install/doinstall.pp
@@ -2,6 +2,7 @@ class selenium_md_oracledb::oracle::install::doinstall {
   require selenium_md_oracledb::oracle::install::prepare
 
 	$path_respfile = "/opt/oradb/database/response/db_install.rsp"
+  $oracleHome = "/opt/oradb/u01/app/oracle/product/11.2.0/dbhome_1"
 	
 	file {'db_install.rsp':
     	ensure  => file,
@@ -11,30 +12,11 @@ class selenium_md_oracledb::oracle::install::doinstall {
   	} ->
 
   	exec { 'installer_oradb':
-  		command      => "sh runInstaller -ignorePrereq -silent -responseFile ${path_respfile}",
+  		command      => "sh runInstaller -ignorePrereq -silent -waitforcompletion -responseFile ${path_respfile}",
   		path        => '/usr/bin',
   		cwd         => '/opt/oradb/database',
   		user        => 'oracle',
-      before      => Exec['configure_oracle_network'],
-  		unless      => 'ls /opt/oradb/u01/',
+      timeout     => 0,
+  		unless      => "ls ${oracleHome}",
   	} 
-
-    ->
-
-    file {'netca.rsp':
-      ensure  => file,
-      path    => '/opt/oradb/database/response/netca.rsp',
-      content => template("selenium_md_oracledb/netca.rsp.erb"),
-      ignore => 'ls /opt/oradb/u01/'
-    } ->
-
-    #não da certo!!!! - Criar um script que aguarde o final da instalação, 
-    #ou alterar o runinstaller
-    exec { 'configure_oracle_network':
-        command      => "sh netca /silent /responseFile ${path_respfile}",
-        path         => '/usr/bin',
-        cwd          => '/opt/oradb/u01/app/oracle/product/11.2.0/dbhome_1/bin/',
-        user         => 'oracle',
-        unless       => 'ls /opt/oradb/u01/'
-      }
 }
