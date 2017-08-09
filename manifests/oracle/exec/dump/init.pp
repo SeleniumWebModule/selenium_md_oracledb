31389eb693d8bcec3e8b237f4613aa551c25dda5
Eduardo Dicarte <eduardo@rjconsultores.com.br>
2017-08-09 14:22:20 -0300

    Inclusão de novas etapas para o dump

--- manifests/oracle/exec/dump/init.pp
+++ manifests/oracle/exec/dump/init.pp
@@ -5,7 +5,12 @@ class selenium_md_oracledb::oracle::exec::dump::init {
 	include selenium_md_oracledb::oracle::exec::dump::schema
 	include selenium_md_oracledb::oracle::exec::dump::types
 	include selenium_md_oracledb::oracle::exec::dump::sequences
-	include selenium_md_oracledb::oracle::exec::dump::createtables
+	include selenium_md_oracledb::oracle::exec::dump::tables
 	include selenium_md_oracledb::oracle::exec::dump::views
 	include selenium_md_oracledb::oracle::exec::dump::indexes
+	include selenium_md_oracledb::oracle::exec::dump::constraints
+	include selenium_md_oracledb::oracle::exec::dump::triggers
+	include selenium_md_oracledb::oracle::exec::dump::functions
+	include selenium_md_oracledb::oracle::exec::dump::packages
+	include selenium_md_oracledb::oracle::exec::dump::procedures
 }
