3889015881d54d7f74f37ee8c57290cf7b2e4ddb
Eduardo Dicarte <eduardo@rjconsultores.com.br>
2017-08-13 01:38:18 -0300

    Alteração na classe para gerar o script de configuração de variáveis de ambiente no rc.d para que o banco de dados esteja disponível na inicialização do sistema

--- manifests/oracle/exec/environment_variables.pp
+++ manifests/oracle/exec/environment_variables.pp
@@ -6,15 +6,15 @@ class selenium_md_oracledb::oracle::exec::environment_variables {
 
 	file {'oracle_env.sh':
 		ensure => file,
-    	path    => '/etc/profile.d/oracle_env.sh',
+    	path    => '/etc/rc.d/oracle_env.sh',
     	owner   => 'root',
     	content => template("selenium_md_oracledb/oracle_env.sh.erb"),
     	mode    => '0775',
   	} -> 
 
   	exec{'run_profile':
-  		command => 'sh /etc/profile.d/oracle_env.sh',
+  		command => 'sh /etc/rc.d/oracle_env.sh',
   		user    => 'root',
-  		path    => '/usr/bin'
+  		path    => '/usr/bin',
   	}
 }
